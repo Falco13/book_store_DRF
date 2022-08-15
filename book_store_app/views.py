535657e1c3c6647e2dfd398bd9b4396000243093
falco13 <keanu0608@gmail.com>
2022-08-15 17:10:11 +0300

    added search and ordering

--- book_store_app/views.py
+++ book_store_app/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 from book_store_app.models import Book
 from book_store_app.serializers import BookSerializer
 
@@ -7,5 +8,7 @@ from book_store_app.serializers import BookSerializer
 class BooksViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.filter(is_active=True)
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
-    filter_fields = ['price', 'created_at', 'category']
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_fields = ['price', 'created_at', 'category__category_title']
+    search_fields = ['title', 'description', 'price', 'category__category_title']
+    ordering_fields = ['price', 'created_at']
