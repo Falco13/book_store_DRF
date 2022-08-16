16e1c429ac2bd231472817ba9331aac17a0487cf
falco13 <keanu0608@gmail.com>
2022-08-17 00:23:55 +0300

    added fields for registration user

--- book_store_app/serializers.py
+++ book_store_app/serializers.py
@@ -12,20 +12,32 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(max_length=25, min_length=8, write_only=True)
+    email = serializers.EmailField(max_length=25, min_length=7)
+    first_name = serializers.CharField(max_length=15, min_length=3)
+    last_name = serializers.CharField(max_length=15, min_length=3)
 
     class Meta:
         model = User
-        fields = ["username", "password", "password2"]
+        fields = ['username', 'password', 'password2', 'email', 'first_name', 'last_name']
         extra_kwargs = {"password": {"write_only": True}}
 
+    def validate(self, attrs):
+        email = attrs.get('email')
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError({'email': 'This email is already in use'})
+        return super().validate(attrs)
+
     def create(self, validated_data):
-        username = validated_data["username"]
-        password = validated_data["password"]
-        password2 = validated_data["password2"]
+        password = validated_data['password']
+        password2 = validated_data['password2']
         if password != password2:
             raise serializers.ValidationError({"password": "Passwords don't match"})
-        user = User(username=username)
+        user = User.objects.create_user(username=validated_data["username"],
+                                        first_name=validated_data["first_name"],
+                                        last_name=validated_data["last_name"],
+                                        email=validated_data["email"],
+                                        )
         user.set_password(password)
         user.save()
         return user
